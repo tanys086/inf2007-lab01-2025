84bfc93527de126338dd307e8a395ffd8fab4a95
tanys086 <2300895@sit.singaporetech.edu.sg>
2025-01-18 14:49:15 +0800

    lab1

--- app/src/main/java/com/inf2007/lab01/MainActivity.kt
+++ app/src/main/java/com/inf2007/lab01/MainActivity.kt
@@ -59,9 +59,6 @@ fun MainScreen() {
                         if (username.isNotBlank()) {
                             showGreeting = true
                         }
-                        else {
-                            showGreeting = false
-                        }
                     },
                     modifier = Modifier
                         .fillMaxWidth()
@@ -92,17 +89,17 @@ fun UserInput(name: String, onNameChange: (String) -> Unit, modifier: Modifier =
         label = { Text("Enter your Name") },
         modifier = modifier
             .fillMaxWidth()
-            .testTag("UserInput")
+            .testTag("nameInput")
     )
 }
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
-        text = "Hello $name!, Welcome to InF2007!",
+        text = "Hello $name!, Welcome to INF2007!",
         modifier = Modifier
             .fillMaxWidth()
-            .testTag("greeting")
+            .testTag("greetingMsg")
     )
 }
 
